59904f0bfd7279f3acb02c0d63ae189358da0be6
Nick Sarlo <nicholas.l.sarlo@gmail.com>
2015-06-15 18:36:23 -0400

    celsius and fahrenheight subclasses created

--- 10_temperature_object/temperature.rb
+++ 10_temperature_object/temperature.rb
@@ -12,4 +12,30 @@ class Temperature
 		@options.key?(:c) ? @options[:c] : (@options[:f] - 32) * 5.0 / 9
 	end
 
+	def self.from_celsius(n)
+		self.new(:c => n)
+	end
+
+	def self.from_fahrenheit(n)
+		self.new(:f => n)
+	end
+
+end
+
+class Celsius < Temperature
+
+	def initialize(n, options = {})
+		@options = options
+		@options[:c] = n
+	end
+
+end
+
+class Fahrenheit < Temperature
+
+	def initialize(n, options = {})
+		@options = options
+		@options[:f] = n
+	end
+
 end
